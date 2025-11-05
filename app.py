2fad73440bd41f970a484bccb2e158ef38d9dd85
André Zauli <andresoares@colegiopaulodetarso.com.br>
2025-11-05 15:51:26 +0000

    rota home

--- app.py
+++ app.py
@@ -18,3 +18,10 @@ def info():
 def bemvindo(usuario):
 
    return f"<h1>Bem-vindo, {usuario.capitalize()}!<h1>"
+from flask import Flask, redirect
+
+@app.get("/home") 
+
+def home(): 
+
+  return redirect("/")
